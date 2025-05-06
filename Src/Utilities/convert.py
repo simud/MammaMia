09bc61ac56ab0ed1d50f6c73549ee2405cb0168c
Simud <149002960+simud@users.noreply.github.com>
2025-05-06 16:14:18 +0200

    Update convert.py

--- Src/Utilities/convert.py
+++ Src/Utilities/convert.py
@@ -1,14 +1,8 @@
-from Src.Utilities.loadenv import load_env
-env_vars = load_env()
-
-TMDB_KEY = env_vars.get('TMDB_KEY')
 async def get_TMDb_id_from_IMDb_id(imdb_id, client):
-    response = await client.get(f'https://api.themoviedb.org/3/find/{imdb_id}', 
-                            params={'external_source': 'imdb_id', 'api_key': f'{TMDB_KEY}'})
-    tmbda = response.json()
-    if tmbda['movie_results']:
-        return tmbda['movie_results'][0]['id']
-    elif tmbda['tv_results']:
-        return tmbda['tv_results'][0]['id']
-    else:
+    # Non possiamo convertire senza TMDb, restituiamo None per IMDb
+    if imdb_id.startswith("tt"):
         return None
+    # Se è un ID TMDb, estraiamo l'ID
+    if "tmdb:" in imdb_id:
+        return imdb_id.replace("tmdb:", "").split(":")[0]
+    return imdb_id
