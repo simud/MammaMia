256faad193385e38f1e0e1e4d9e4546dac900be5
Simud <149002960+simud@users.noreply.github.com>
2025-05-06 16:11:21 +0200

    Update config.py

--- Src/Utilities/config.py
+++ Src/Utilities/config.py
@@ -1,12 +1,8 @@
-#LOAD THE CONFIG
 import json
 
-# Open the configuration file
 with open('config.json') as f:
-    # Load JSON data from file
     config = json.load(f)
 
-# Accessing SC_DOMAIN
 SITE = config["Siti"]
 FT_DOMAIN = SITE["Filmpertutti"]['domain']
 SC_DOMAIN = SITE["StreamingCommunity"]['domain']
@@ -17,7 +13,6 @@ AW_DOMAIN = SITE['AnimeWorld']['domain']
 SKY_DOMAIN = SITE['SkyStreaming']['domain']
 MYSTERIUS = SITE['Mysterius']['enabled']
 DLHD = SITE['DaddyLiveHD']['enabled']
-#General
 GENERAL = config['General']
 dotenv = GENERAL["load_env"]
 HOST = GENERAL["HOST"]
