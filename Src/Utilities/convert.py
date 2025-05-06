039d3625089122c1b4bab61f7a3baab10973330c
Simud <149002960+simud@users.noreply.github.com>
2025-05-06 16:10:58 +0200

    Update convert.py

--- Src/Utilities/convert.py
+++ Src/Utilities/convert.py
@@ -2,7 +2,7 @@ from Src.Utilities.loadenv import load_env
 env_vars = load_env()
 
 TMDB_KEY = env_vars.get('TMDB_KEY')
-async def get_TMDb_id_from_IMDb_id(imdb_id,client):
+async def get_TMDb_id_from_IMDb_id(imdb_id, client):
     response = await client.get(f'https://api.themoviedb.org/3/find/{imdb_id}', 
                             params={'external_source': 'imdb_id', 'api_key': f'{TMDB_KEY}'})
     tmbda = response.json()
